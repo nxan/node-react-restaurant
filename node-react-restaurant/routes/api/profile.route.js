b0d71b26caa6166eabbaf4e38e01a9b5d9c67cc4
nxan <nxan.developer@gmail.com>
2019-06-01 10:46:56 +0700

    Update profile.route.js

--- node-react-restaurant/routes/api/profile.route.js
+++ node-react-restaurant/routes/api/profile.route.js
@@ -7,6 +7,11 @@ const { check, validationResult } = require('express-validator/check');
 const Profile = require('../../model/Profile');
 const User = require('../../model/User');
 
+
+/* ----- 
+  @route  GET api/profile
+  @desc   Get logged in profile
+-----*/
 Profile.hasOne(User, { foreignKey: 'id', sourceKey: 'user_id' });
 
 router.get('/me', auth, async (req, res) => {
@@ -27,6 +32,11 @@ router.get('/me', auth, async (req, res) => {
     }
 });
 
+/* ----- 
+  @route  POST api/profile
+  @desc   Create or update user profile
+-----*/
+
 router.post('/', [auth, [
     check('shop_url', 'Shop URL is required').not().isEmpty()
 ]], async (req, res) => {
@@ -37,6 +47,75 @@ router.post('/', [auth, [
 
     const { shop_url, shop_name } = req.body;
 
+    //Build profile object
+    var profileFields = {};
+    profileFields.user_id = req.user.id;
+    if (shop_url) profileFields.shop_url = shop_url;
+    if (shop_name) profileFields.shop_name = shop_name;
+    try {
+        var profile = await Profile.findOne({
+            include: [{
+                model: User,
+                where: { id: req.user.id }
+            }]
+        });
+        if (profile) {
+            profile.update({
+                attributes: ['id', 'shop_url', 'shop_name'],
+                shop_url: profileFields.shop_url,
+                shop_name: profileFields.shop_name
+            });
+            return res.json(profile);
+        } else {
+            profile = new Profile(profileFields);
+            await profile.save();
+            res.json(profile);
+        }
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+
+});
+
+/* ----- 
+  @route  GET api/profile
+  @desc   Get all profile
+-----*/
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.findAll({
+            include: [{
+                model: User
+            }]
+        });
+        res.json(profiles)
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+/* ----- 
+  @route  GET api/profile//user/:user_id
+  @desc   Get user profile
+-----*/
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            include: [{
+                model: User,
+                where: { id: req.params.user_id }
+            }]
+        });
+        if (!profile) {
+            return res.status(400).json({ message: 'Profile not found' });
+        }
+        res.json(profile)
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server error');
+    }
 });
 
 module.exports = router;
